d5ccdf53743fde433768d5e748a3f3f2f0815867
igor.kudlo <igor.kudlo@spiralscout.com>
2022-08-25 12:51:05 +0300

    Task 02

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,29 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const button = document.querySelector('.collapsible__button');
+const buttonActionVisible = button.querySelector('.collapsible__action--visible');
+const buttonActionHidden = button.querySelector('.collapsible__action--hidden');
+const content = document.querySelector('.collapsible__content');
+
+buttonActionVisible.style.display = 'none';
+content.style.transform = 'translate3D(0, -100%, 0)';
+content.style.opacity = '0';
+
+let collapsibleOpen = false;
+
+button.addEventListener('click', () => {
+    collapsibleOpen = !collapsibleOpen;
+
+    if (collapsibleOpen) {
+        buttonActionHidden.style.display = 'none';
+        buttonActionVisible.style.display = 'block';
+        content.animate(toggleContent, { duration: 200, fill: 'forwards' }).commitStyles();
+    } else {
+        buttonActionVisible.style.display = 'none';
+        buttonActionHidden.style.display = 'block';
+        content.animate(toggleContent, { duration: 200, direction: 'reverse', fill: 'forwards' }).commitStyles();
+    }
+});
+
+const toggleContent = [
+    { transform: 'translate3D(0, -100%, 0)', opacity: '0' },
+    { transform: 'translate3D(0, 0, 0)', opacity: '1' }
+];
